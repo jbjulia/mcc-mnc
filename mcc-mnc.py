242a104e4229c1de3ebdc3f907d0807fde819d3d
jbjulia <joseph.b.julian@gmail.com>
2021-06-20 14:19:34 -0500

    Major updates and improvements

--- mcc-mnc.py
+++ mcc-mnc.py
@@ -5,84 +5,166 @@ belongs to. To uniquely identify a mobile subscriber's network provider
 the MCC is combined with a Mobile Network Code (MNC). The combination
 of MCC and MNC is called the Home Network Identity (HNI), and is the
 concatenation of both strings (e.g. MCC = 262 and MNC = 01 results in
-an HNI of 26201). Combining the HNI with the MSIN (Mobile Subscription
+a HNI of 26201). Combining the HNI with the MSIN (Mobile Subscription
 Identification Number) results in the International Mobile Subscriber
 Identity (IMSI), from which a subscriber can be further identified.
+This tool is the Pythonic equivalent of https://www.mcc-mnc.com/.
 """
 
 import csv
 import json
+import os
 import sys
+import urllib.error
+import urllib.request
 import uuid
 
 
 def main(argv):
     """
-    Takes passed arg(s) and calls respective function. If no valid arg
-    is passed, displays list of acceptable args and example of use.
-    :param argv:
+    Takes passed args and calls respective function. If no valid arg is
+    passed, prints list of acceptable args and examples of use.
+    :param argv: User's desired arguments
     :return:
     """
     arg_list = [
-        # "-cc:  Country Code"
-        "-mcc:  Mobile Country Code",
-        "-mnc:  Mobile Network Code"
+        "-cc ##:  Returns countries matching Country Code (CC) "
+        "(e.g. -cc 93)",
+        "-mcc ###:  Returns countries matching Mobile Country Code (MCC) "
+        "(e.g. -mcc 412)",
+        "-mcc ### -mnc ##:  Returns country matching Mobile Country Code (MCC) and Mobile Network Code (MNC) "
+        "(e.g. -mcc 412 -mnc 88)",
+        "-cc ## -mcc ### -mnc ##:  "
+        "Returns country matching Country Code (CC), Mobile Country Code (MCC) and Mobile Network Code (MNC) "
+        "(e.g. -cc 93 -mcc 412 -mnc 88)",
+        "-update:  Downloads and refreshes local CSV and JSON"
     ]
     try:
         if argv == "-help":
-            print("Please refer to to project repository for documentation:  https://github.com/jbjulia/mcc-mnc/")
-        elif argv[0].lower() == "-mcc" and not argv[2]:
-            identify(argv[1])
+            print("Please refer to the project repository for documentation:  https://github.com/jbjulia/mcc-mnc/")
+        if argv.lower() == "-update":
+            ingest_csv()
+        elif argv[0].lower() == "-cc" and len(argv) < 3:
+            identify(user_cc=argv[1])  # User's desired CC
+        elif argv[0].lower() == "-mcc" and len(argv) < 3:
+            identify(user_mcc=argv[1])  # User's desired MCC
         elif argv[0].lower() == "-mcc" and argv[2].lower() == "-mnc":
-            identify(argv[1].lower(), argv[3].lower())
+            identify(user_mcc=argv[1], user_mnc=argv[3])  # User's desired MCC and MNC
+        elif argv[0].lower() == "-cc" and argv[2].lower() == "-mcc" and argv[4].lower() == "-mnc":
+            identify(user_cc=argv[1], user_mcc=argv[3], user_mnc=argv[5])  # User's desired CC, MCC and MNC
         else:
-            print("Argument not recognized.\n\nAcceptable arguments:")
+            print("Error: Invalid arguments.\n\nAcceptable arguments:")
             for item in arg_list:
                 print(f"\t{item}")
-            print("\nExample 1:  mcc-mnc.py -mcc 544 -mnc 11")
+            print("\nExample:  mcc-mnc.py -cc 93 -mcc 412 -mnc 88")
     except IndexError:
-        print("Incorrect argument format.")
+        print("Error: Incorrect argument format.")
         sys.exit(1)
 
 
-def identify(user_mcc, user_mnc=None):
+def identify(user_cc=None, user_mcc=None, user_mnc=None):
     """
-    Attempts to identify the country of which matches the passed MCC
-    and MNC. Prints respective MCC-MNC data if match is found.
-    :param user_mcc:
-    :param user_mnc:
+    Attempts to identify the country of which matches the passed CC
+    and/or MCC/MNC. Prints respective MCC-MNC data if match is found.
+    :param user_cc: User's desired Country Code (CC)
+    :param user_mcc: User's desired Mobile Country Code (MCC)
+    :param user_mnc: User's desired Mobile Network Code (MNC)
     :return:
     """
     match_found = False
+    match_country = None
+    match_list = []
+    match_count = 0
     try:
         with open("data/mcc-mnc.json", "r") as json_file:
             json_data = json.load(json_file)
-        if not user_mnc:
+        if user_cc and not user_mcc and not user_mnc:
             for country in json_data.keys():
-                if user_mcc in json_data[country]["MCC"]:
+                if user_cc == json_data[country]["CC"]:
                     match_found = True
-                    print(f"Match found:  MCC [{user_mcc}] exists in {country}")
-        else:
+                    match_country = country
+                    match_list.append(match_country)
+                    match_count += 1
+            if match_found:
+                print(f"{match_count} Matches Found:  CC [{user_cc}] exists in {match_country.split('-')[0]}.\n")
+                for country in json_data.keys():
+                    if country in match_list:
+                        for key, val in json_data[country].items():
+                            print(f"\t{key}: {val}")
+                        print("\n")
+        elif user_mcc and not user_cc and not user_mnc:
             for country in json_data.keys():
-                if user_mcc in json_data[country]["MCC"] and user_mnc in json_data[country]["MNC"]:
+                if user_mcc == json_data[country]["MCC"]:
                     match_found = True
-                    print(f"Match found:  MCC [{user_mcc}] and MNC [{user_mnc}] exists in {country.split('-')[0]}\n")
+                    match_country = country
+                    match_list.append(match_country)
+                    match_count += 1
+            if match_found:
+                print(f"{match_count} Matches Found:  MCC [{user_mcc}] exists in {match_country.split('-')[0]}.\n")
+                for country in json_data.keys():
+                    if country in match_list:
+                        for key, val in json_data[country].items():
+                            print(f"\t{key}: {val}")
+                        print("\n")
+        elif user_mcc and user_mnc and not user_cc:
+            for country in json_data.keys():
+                if user_mcc == json_data[country]["MCC"] and user_mnc == json_data[country]["MNC"]:
+                    match_found = True
+                    print(f"Match Found:  MCC [{user_mcc}] and MNC [{user_mnc}] exists in {country.split('-')[0]}, "
+                          f"with a Home Network Identity (HNI) of [{user_mcc + user_mnc}].\n")
                     for key, val in json_data[country].items():
-                        print(f"{key}: {val}")
+                        print(f"\t{key}: {val}")
+        elif user_cc and user_mcc and user_mnc:
+            for country in json_data.keys():
+                if user_cc == json_data[country]["CC"] and \
+                        user_mcc == json_data[country]["MCC"] and \
+                        user_mnc == json_data[country]["MNC"]:
+                    match_found = True
+                    print(f"Match Found:  {country.split('-')[0]}.\n")
+                    for key, val in json_data[country].items():
+                        print(f"\t{key}: {val}")
         if not match_found:
-            print("No match found.")
+            print("No Match Found.")
     except(IndexError, KeyError, ValueError) as e:
-        print(e)
+        print(f"Error: The following exception has occurred: {e}")
         sys.exit(1)
+    finally:
+        print(os.get_terminal_size().columns * "-")
+        sys.exit(0)
 
 
 def ingest_csv():
     """
-    Reads in target CSV file containing current MCC-MNC data. Writes
-    data to target JSON file and exits after reformatting.
+    Attempts to download CSV from project repository. Checks if CSV
+    file exists, removes if true. Creates new CSV and writes data
+    decoded from raw URL. Checks if JSON file already exists, removes
+    if true. Creates new JSON file and writes empty dictionary. Reads
+    in target CSV file containing current MCC-MNC data. Writes data to
+    target JSON file and exits after reformatting. Requires network.
     :return:
     """
     try:
+        with urllib.request.urlopen("https://raw.githubusercontent.com/jbjulia/mcc-mnc/master/data/mcc-mnc.csv") as raw:
+            print(f"Decoding raw CSV {'https://raw.githubusercontent.com/jbjulia/mcc-mnc/master/data/mcc-mnc.csv'}.")
+            data = raw.read().decode("utf-8")
+        if os.path.exists("data/mcc-mnc.csv"):
+            print(f"Removing old CSV {'data/mcc-mnc.csv'}.")
+            os.remove("data/mcc-mnc.csv")
+        print(f"Creating new CSV {'data/mcc-mnc.csv'}.")
+        f = open("data/mcc-mnc.csv", "w+")
+        print(f"Writing CSV data to {'data/mcc-mnc.csv'}.")
+        f.write(f"{data}")
+        print(f"Closing {'data/mcc-mnc.csv'}.")
+        f.close()
+        if os.path.exists("data/mcc-mnc.json"):
+            print(f"Removing old JSON {'data/mcc-mnc.json'}.")
+            os.remove("data/mcc-mnc.json")
+        print(f"Creating new JSON {'data/mcc-mnc.csv'}.")
+        f = open("data/mcc-mnc.json", "w+")
+        print(f"Writing empty JSON dictionary to {'data/mcc-mnc.json'}.")
+        f.write("{}\r\n")
+        print(f"Closing {'data/mcc-mnc.json'}.")
+        f.close()
         with open("data/mcc-mnc.json", "r") as json_file:
             json_data = json.load(json_file)
         with open("data/mcc-mnc.csv", "r") as csv_file:
@@ -113,13 +195,20 @@ def ingest_csv():
                     )
         with open("data/mcc-mnc.json", "w") as out_file:
             json.dump(json_data, out_file, indent=4, sort_keys=True)
+            print(f"\nSuccessfully updated JSON file: {os.path.getsize('data/mcc-mnc.json')} bytes.")
+    except OSError as e:
+        print(f"Error: Please try again as Administrator: {e}")
+        sys.exit(1)
+    except urllib.error as e:
+        print(f"Error: Unable to resolve URL: {e}")
+        sys.exit(1)
     except(IndexError, KeyError, ValueError) as e:
-        print(e)
+        print(f"Error: The following exception has occurred: {e}")
         sys.exit(1)
     finally:
+        print(os.get_terminal_size().columns * "-")
         sys.exit(0)
 
 
 if __name__ == "__main__":
-    # ingest_csv()  # Un-hash to refresh 'mcc-mnc.json' file
     main(sys.argv[1:] if sys.argv[1:] else "-help")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mcc-mnc',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/jbjulia/mcc-mnc',
+    license='MIT',
+    author='Joseph Julian',
+    author_email='joseph.b.julian@gmail.com',
+    description='Mobile Country Codes (MCC) and Mobile Network Codes (MNC)'
+)
