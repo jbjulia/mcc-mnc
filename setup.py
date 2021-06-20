1290476535a3786515bdc92a6189afd327a7c816
jbjulia <joseph.b.julian@gmail.com>
2021-06-20 16:21:26 -0500

    Code cleanup

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name='mcc-mnc',
     version='1.0.0',
@@ -12,5 +9,4 @@ setup(
     author='Joseph Julian',
     author_email='joseph.b.julian@gmail.com',
     description='Mobile Country Codes (MCC) and Mobile Network Codes (MNC)',
-    long_description=long_description
 )
